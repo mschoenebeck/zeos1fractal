d0ba9bb06a5dc80f9dcc11c93106197d193e8949
n0umen0n <46645013+n0umen0n@users.noreply.github.com>
2023-08-23 10:55:57 +0300

    handling edgecase of less than 3 participants

--- tests/zeos1fractal.cpp
+++ tests/zeos1fractal.cpp
@@ -392,93 +392,101 @@ void zeos1fractal::create_groups()
         // Add each participant's name to the all_participants vector
         all_participants.push_back(participant.user);
     }
-
-    // Shuffle the all_participants vector
-    rng_t rndnmbr("r4ndomnumb3r"_n, "r4ndomnumb3r"_n.value);
-    checksum256 x = rndnmbr.get().value;
-    uint32_t seed = *reinterpret_cast<uint32_t*>(&x);
-    my_shuffle(all_participants.begin(), all_participants.end(), seed);
-
-    rooms_t rooms(_self, _self.value);
-    auto num_participants = all_participants.size();
-
-    vector<uint8_t> group_sizes;
-
-    // First part: hardcoded groups up to 20
-    if (num_participants <= 20)
+    if(all_participants.size() < 3) // Minimum number of participants could be also put in _global
     {
-        if (num_participants == 3) group_sizes = {3};
-        else if (num_participants == 4) group_sizes = {4};
-        else if (num_participants == 5) group_sizes = {5};
-        else if (num_participants == 6) group_sizes = {6};
-        else if (num_participants == 7) group_sizes = {3, 4};
-        else if (num_participants == 8) group_sizes = {4, 4};
-        else if (num_participants == 9) group_sizes = {5, 4};
-        else if (num_participants == 10) group_sizes = {5, 5};
-        else if (num_participants == 11) group_sizes = {5, 6};
-        else if (num_participants == 12) group_sizes = {6, 6};
-        else if (num_participants == 13) group_sizes = {5, 4, 4};
-        else if (num_participants == 14) group_sizes = {5, 5, 4};
-        else if (num_participants == 15) group_sizes = {5, 5, 5};
-        else if (num_participants == 16) group_sizes = {6, 5, 5};
-        else if (num_participants == 17) group_sizes = {6, 6, 5};
-        else if (num_participants == 18) group_sizes = {6, 6, 6};
-        else if (num_participants == 19) group_sizes = {5, 5, 5, 4};
-        else if (num_participants == 20) group_sizes = {5, 5, 5, 5};
+        auto g = _global.get();
+        g.state = STATE_IDLE;
+        g.next_event_block_height = g.next_event_block_height + 1209600; // add one week of blocks
     }
-
-    // Second part: generic algorithm for 20+ participants
     else
-    {
-        // As per the constraint, we start with groups of 6
-        uint8_t count_of_fives = 0;
-        while (num_participants > 0)
+    {  
+        // Shuffle the all_participants vector
+        rng_t rndnmbr("r4ndomnumb3r"_n, "r4ndomnumb3r"_n.value);
+        checksum256 x = rndnmbr.get().value;
+        uint32_t seed = *reinterpret_cast<uint32_t*>(&x);
+        my_shuffle(all_participants.begin(), all_participants.end(), seed);
+
+        rooms_t rooms(_self, _self.value);
+        auto num_participants = all_participants.size();
+
+        vector<uint8_t> group_sizes;
+
+        // First part: hardcoded groups up to 20
+        if (num_participants <= 20)
         {
-            if (num_participants % 6 != 0 && count_of_fives < 5)
-            {
-                group_sizes.push_back(5);
-                num_participants -= 5;
-                count_of_fives++;
-            }
-            else
-            {
-                group_sizes.push_back(6);
-                num_participants -= 6;
-                if (count_of_fives == 5) count_of_fives = 0;
-            }
-        }
-    }
-
-    // Create the actual groups using group_sizes
-    auto iter = all_participants.begin();
-
-    uint64_t room_id = rooms.available_primary_key();
-    // Ensure we start from 1 if table is empty
-    if (room_id == 0) 
-    {
-        room_id = 1;
-    }
-
-    // Iterate over the group sizes to create and populate rooms
-    for (uint8_t size : group_sizes)
-    {
-        // A temporary vector to store the users in the current room
-        vector<name> users_in_room;
-
-        // Iterate until the desired group size is reached or until all participants have been processed
-        for (uint8_t j = 0; j < size && iter != all_participants.end(); j++, ++iter)
-        {
-            // Add the current participant to the users_in_room vector
-            users_in_room.push_back(*iter);
+            if (num_participants == 3) group_sizes = {3};
+            else if (num_participants == 4) group_sizes = {4};
+            else if (num_participants == 5) group_sizes = {5};
+            else if (num_participants == 6) group_sizes = {6};
+            else if (num_participants == 7) group_sizes = {3, 4};
+            else if (num_participants == 8) group_sizes = {4, 4};
+            else if (num_participants == 9) group_sizes = {5, 4};
+            else if (num_participants == 10) group_sizes = {5, 5};
+            else if (num_participants == 11) group_sizes = {5, 6};
+            else if (num_participants == 12) group_sizes = {6, 6};
+            else if (num_participants == 13) group_sizes = {5, 4, 4};
+            else if (num_participants == 14) group_sizes = {5, 5, 4};
+            else if (num_participants == 15) group_sizes = {5, 5, 5};
+            else if (num_participants == 16) group_sizes = {6, 5, 5};
+            else if (num_participants == 17) group_sizes = {6, 6, 5};
+            else if (num_participants == 18) group_sizes = {6, 6, 6};
+            else if (num_participants == 19) group_sizes = {5, 5, 5, 4};
+            else if (num_participants == 20) group_sizes = {5, 5, 5, 5};
         }
 
-        // Insert a new room record into the rooms table
-        rooms.emplace(_self, [&](auto& r) {
-            // Assign a unique ID to the room and prepare for the next room's ID
-            r.id = room_id++;
-            // Assign the users_in_room vector to the current room's user list
-            r.users = users_in_room;
-        });
+        // Second part: generic algorithm for 20+ participants
+        else
+        {
+            // As per the constraint, we start with groups of 6
+            uint8_t count_of_fives = 0;
+            while (num_participants > 0)
+            {
+                if (num_participants % 6 != 0 && count_of_fives < 5)
+                {
+                    group_sizes.push_back(5);
+                    num_participants -= 5;
+                    count_of_fives++;
+                }
+                else
+                {
+                    group_sizes.push_back(6);
+                    num_participants -= 6;
+                    if (count_of_fives == 5) count_of_fives = 0;
+                }
+            }
+        }
+
+        // Create the actual groups using group_sizes
+        auto iter = all_participants.begin();
+
+        uint64_t room_id = rooms.available_primary_key();
+        // Ensure we start from 1 if table is empty
+        if (room_id == 0) 
+        {
+            room_id = 1;
+        }
+
+        // Iterate over the group sizes to create and populate rooms
+        for (uint8_t size : group_sizes)
+        {
+            // A temporary vector to store the users in the current room
+            vector<name> users_in_room;
+
+            // Iterate until the desired group size is reached or until all participants have been processed
+            for (uint8_t j = 0; j < size && iter != all_participants.end(); j++, ++iter)
+            {
+                // Add the current participant to the users_in_room vector
+                users_in_room.push_back(*iter);
+            }
+
+            // Insert a new room record into the rooms table
+            rooms.emplace(_self, [&](auto& r) {
+                // Assign a unique ID to the room and prepare for the next room's ID
+                r.id = room_id++;
+                // Assign the users_in_room vector to the current room's user list
+                r.users = users_in_room;
+            });
+        }
     }
 }
 
